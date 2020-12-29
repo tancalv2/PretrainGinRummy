dc76c27d454354972f9a7b1d0b1b6028870ac74f
tancalv2 <calvinkh.tan@mail.utoronto.ca>
2020-12-28 20:39:00 -0500

    Add parameter text file generation
    Generate a text file of all parameters used to train model

--- SupervisedLearning/utils.py
+++ SupervisedLearning/utils.py
@@ -1,4 +1,5 @@
 import os
+from shutil import copyfile
 
 # Available states-action pairs:
 state_action_pair = {'all': 'all', # all actions
@@ -7,17 +8,18 @@ state_action_pair = {'all': 'all', # all actions
                     'apad': 'knock_bin'} # binary action
 
 # All Possible Classes
-all_classes = ['SP0','SP1','Draw','Pickup','DH','GIN',
-               'AS_D', '2S_D', '3S_D', '4S_D', '5S_D', '6S_D', '7S_D', '8S_D', '9S_D', 'TS_D', 'JS_D', 'QS_D', 'KS_D',
-               'AH_D', '2H_D', '3H_D', '4H_D', '5H_D', '6H_D', '7H_D', '8H_D', '9H_D', 'TH_D', 'JH_D', 'QH_D', 'KH_D',
-               'AD_D', '2D_D', '3D_D', '4D_D', '5D_D', '6D_D', '7D_D', '8D_D', '9D_D', 'TD_D', 'JD_D', 'QD_D', 'KD_D',
-               'AC_D', '2C_D', '3C_D', '4C_D', '5C_D', '6C_D', '7C_D', '8C_D', '9C_D', 'TC_D', 'JC_D', 'QC_D', 'KC_D',
-               'AS_K', '2S_K', '3S_K', '4S_K', '5S_K', '6S_K', '7S_K', '8S_K', '9S_K', 'TS_K', 'JS_K', 'QS_K', 'KS_K',
-               'AH_K', '2H_K', '3H_K', '4H_K', '5H_K', '6H_K', '7H_K', '8H_K', '9H_K', 'TH_K', 'JH_K', 'QH_K', 'KH_K',
-               'AD_K', '2D_K', '3D_K', '4D_K', '5D_K', '6D_K', '7D_K', '8D_K', '9D_K', 'TD_K', 'JD_K', 'QD_K', 'KD_K',
-               'AC_K', '2C_K', '3C_K', '4C_K', '5C_K', '6C_K', '7C_K', '8C_K', '9C_K', 'TC_K', 'JC_K', 'QC_K', 'KC_K']
+all_classes = ['SP0','SP1','DRW','PU','DH','GIN',
+               'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS',
+               'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH',
+               'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD',
+               'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC',
+               'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS',
+               'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH',
+               'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD',
+               'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC']
 
 
+################################################# Create Directories #################################################
 def create_dir(pth, state, action, model_name):
 	# create model and plot directories if do not exist
 
@@ -47,3 +49,48 @@ def create_dir(pth, state, action, model_name):
 	data_pth = '{}/data/{}/{}'.format(pth,state,action)
 
 	return data_pth, model_pth, plot_pth
+
+
+################################################# Write Parameters #################################################
+def write_params(pth, state, action, model_name, numGames, pruneStatesList, actionChoice, balance, batch_size, lr, epochs, model_fnc, activation, loss, loss_weight, pre_train, model_PT, device):
+	
+	model_pth = '{}/models/{}'.format(pth,state)
+	model_pth = '{}/{}'.format(model_pth,action)
+	model_pth = '{}/{}.txt'.format(model_pth,model_name)
+
+	plot_pth = '{}/plots/{}'.format(pth,state)
+	plot_pth = '{}/{}'.format(plot_pth,action)
+	plot_pth = '{}/{}.txt'.format(plot_pth,model_name)
+
+	# Delete old version from model directory
+	if os.path.exists(model_pth):
+  		os.remove(model_pth)
+
+  	# Write params into txt file (add onto list as parameters increase)
+	with open(model_pth, 'w') as f:
+		f.write('State: {}\n'.format(state))
+		f.write('Action: {}\n\n'.format(action))
+		f.write('model_name: {}\n\n'.format(model_name))
+		f.write('numGames: {}\n\n'.format(numGames))
+		f.write('pruneStatesList: {}\n'.format(pruneStatesList))
+		f.write('actionChoice: {}\n'.format(actionChoice))
+		f.write('balance: {}\n\n'.format(balance))
+		f.write('batch_size: {}\n'.format(batch_size))
+		f.write('lr: {}\n'.format(lr))
+		f.write('epochs: {}\n\n'.format(epochs))
+		f.write('model_fnc: {}\n'.format(model_fnc))
+		f.write('activation: {}\n\n'.format(activation))
+		f.write('loss: {}\n'.format(loss))
+		f.write('loss_weight: {}\n\n'.format(loss_weight))
+		f.write('pre_train: {}\n'.format(pre_train))
+		f.write('model_PT: {}\n\n'.format(model_PT))
+		f.write('device: {}\n'.format(device))
+		f.close()
+
+	# Delete old version from plots directory
+	if os.path.exists(plot_pth):
+  		os.remove(plot_pth)
+
+	# Copy params txt file to plots directory
+	copyfile(model_pth, plot_pth)
+	print('Parameters written.')

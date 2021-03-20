e79b21f33bbc9b5ce06af6ec49a04ffb3285280d
tancalv2 <calvinkh.tan@mail.utoronto.ca>
2021-03-20 14:13:42 -0400

    Added additional models
    MLP_4HL:
    [input_size*2, input_size*4, input_size*4, , input_size*2]
    
    MLP_4HL2:
    input_size*2, input_size*2, input_size*2, , input_size*2
    
    MLP_2HL_wide:
    input_size*2, input_size*2

--- SupervisedLearning/models.py
+++ SupervisedLearning/models.py
@@ -74,3 +74,90 @@ class MLP_3HL(nn.Module):
         x = self.act_fnc(x)
         x = self.l4(x)
         return self.sfx(x)
+
+class MLP_4HL(nn.Module):
+    '''
+    4 Hidden Layers: size input_size*2, input_size*4, input_size*4, , input_size*2
+    '''
+    def __init__(self, input_size, output_size, activation='sig'):
+        super(MLP_4HL, self).__init__()
+        self.l1 = nn.Linear(input_size, input_size*2)
+        self.l2 = nn.Linear(input_size*2, input_size*4)
+        self.l3 = nn.Linear(input_size*4, input_size*4)
+        self.l4 = nn.Linear(input_size*4, input_size*2)
+        self.l5 = nn.Linear(input_size*2, output_size)
+        if activation == 'relu':
+            self.act_fnc = nn.ReLU()
+        elif activation == 'tanh':
+            self.act_fnc = nn.Tanh()
+        else:
+            self.act_fnc = nn.Sigmoid()
+        self.sfx = nn.Softmax(dim=1)
+
+    def forward(self, features):
+        x = self.l1(features)
+        x = self.act_fnc(x)
+        x = self.l2(x)
+        x = self.act_fnc(x)
+        x = self.l3(x)
+        x = self.act_fnc(x)
+        x = self.l4(x)
+        x = self.act_fnc(x)
+        x = self.l5(x)
+        return self.sfx(x)
+
+class MLP_4HL2(nn.Module):
+    '''
+    4 Hidden Layers: size input_size*2, input_size*2, input_size*2, , input_size*2
+    '''
+    def __init__(self, input_size, output_size, activation='sig'):
+        super(MLP_4HL2, self).__init__()
+        self.l1 = nn.Linear(input_size, input_size*2)
+        self.l2 = nn.Linear(input_size*2, input_size*2)
+        self.l3 = nn.Linear(input_size*2, input_size*2)
+        self.l4 = nn.Linear(input_size*2, input_size*2)
+        self.l5 = nn.Linear(input_size*2, output_size)
+        if activation == 'relu':
+            self.act_fnc = nn.ReLU()
+        elif activation == 'tanh':
+            self.act_fnc = nn.Tanh()
+        else:
+            self.act_fnc = nn.Sigmoid()
+        self.sfx = nn.Softmax(dim=1)
+
+    def forward(self, features):
+        x = self.l1(features)
+        x = self.act_fnc(x)
+        x = self.l2(x)
+        x = self.act_fnc(x)
+        x = self.l3(x)
+        x = self.act_fnc(x)
+        x = self.l4(x)
+        x = self.act_fnc(x)
+        x = self.l5(x)
+        return self.sfx(x)
+
+class MLP_2HL_wide(nn.Module):
+    '''
+    2 Hidden Layers: size input_size*2, input_size*2
+    '''
+    def __init__(self, input_size, output_size, activation='sig'):
+        super(MLP_2HL_wide, self).__init__()
+        self.l1 = nn.Linear(input_size, input_size*4)
+        self.l2 = nn.Linear(input_size*4, input_size*4)
+        self.l3 = nn.Linear(input_size*4, output_size)
+        if activation == 'relu':
+            self.act_fnc = nn.ReLU()
+        elif activation == 'tanh':
+            self.act_fnc = nn.Tanh()
+        else:
+            self.act_fnc = nn.Sigmoid()
+        self.sfx = nn.Softmax(dim=1)
+
+    def forward(self, features):
+        x = self.l1(features)
+        x = self.act_fnc(x)
+        x = self.l2(x)
+        x = self.act_fnc(x)
+        x = self.l3(x)
+        return self.sfx(x)
